71963890ff2ba5381cece5e7507f6c4937d85526
slezkinis <slezkin.is@mail.ru>
2023-02-05 15:35:59 +0300

    Add static_root

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -112,11 +112,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = Path(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
