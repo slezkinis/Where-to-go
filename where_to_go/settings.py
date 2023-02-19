7e1614e504bfc983155f03ce0f4dd3a6b528a14d
slezkinis <slezkin.is@mail.ru>
2023-02-19 18:22:22 +0300

    Add STATIC_ROOT

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -120,3 +120,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = Path(BASE_DIR, 'static')
